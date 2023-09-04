f6a351a680aa54d7bec73353250bf8a01422e010
Bright Nkrumah <brytey2k@gmail.com>
2023-09-04 18:45:39 +0000

    Tests: tests to check validity of options passed to WatchLessonCommand

--- tests/Feature/WatchLessonCommandTest.php
+++ tests/Feature/WatchLessonCommandTest.php
@@ -20,4 +20,26 @@ class WatchLessonCommandTest extends TestCase
         $output->expectsOutput('Lesson watched');
         $output->assertExitCode(0);
     }
+
+    public function testMissingUserWillGiveAnError(): void
+    {
+        $this->seed();
+
+        $output = $this->artisan('app:watch-lesson', [
+            '--user' => 9999953
+        ]);
+
+        $output->expectsOutput('User not found');
+    }
+
+    public function testMissingLessonWillGiveAnError(): void
+    {
+        $this->seed();
+
+        $output = $this->artisan('app:watch-lesson', [
+            '--lesson' => 9999953
+        ]);
+
+        $output->expectsOutput('Lesson not found');
+    }
 }

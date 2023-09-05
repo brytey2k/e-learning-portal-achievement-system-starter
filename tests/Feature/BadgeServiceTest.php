802b29a1a4ec72991a150f9645abdf6e6fc33aea
Bright Nkrumah <brytey2k@gmail.com>
2023-09-05 13:25:14 +0000

    Tests: adds tests for BadgeService class

---
+++ tests/Feature/BadgeServiceTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Events\BadgeUnlocked;
+use App\Libraries\Services\BadgeService;
+use App\Models\Badge;
+use App\Models\User;
+use Event;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BadgeServiceTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function testNewBadgeIsNotAssignedAfterLastBadgeIsReached(): void
+    {
+        Event::fake(BadgeUnlocked::class);
+        $user = User::factory()->create([
+            'badge_id' => Badge::factory()->create(['achievements_required' => 0])->id,
+        ]);
+
+        $this->assertEmpty((new BadgeService())->unlockNextBadge($user));
+        Event::assertNotDispatched(BadgeUnlocked::class);
+    }
+}

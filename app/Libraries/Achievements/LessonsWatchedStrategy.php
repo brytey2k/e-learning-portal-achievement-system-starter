eef6755125b82d0ea79e6f36f604159678d7ac44
Bright Nkrumah <brytey2k@gmail.com>
2023-09-05 21:57:10 +0000

    Feature: adds logging for achievement unlocking strategies

--- app/Libraries/Achievements/LessonsWatchedStrategy.php
+++ app/Libraries/Achievements/LessonsWatchedStrategy.php
@@ -4,11 +4,36 @@ namespace App\Libraries\Achievements;
 
 use App\Models\Achievement;
 use App\Models\User;
+use Psr\Log\LoggerInterface;
 
 class LessonsWatchedStrategy extends AchievementUnlockStrategy
 {
+
+    protected LoggerInterface $logger;
+
+    public function __construct()
+    {
+        $this->logger = app(LoggerInterface::class);
+    }
+
     public function checkCriteria(User $user, Achievement $achievement): bool
     {
-        return $user->watched()->count() >= $achievement->target_count;
+        $this->logger->info('Checking criteria for achievement', ['user_id' => $user->id, 'achievement_id' => $achievement->id, 'achievementType' => $achievement->type]);
+
+        $hasAchieved = $user->watched()->count() >= $achievement->target_count;
+
+        $this->logger->log(
+            $hasAchieved ? 'info' : 'notice',
+            $hasAchieved ? 'User meets requirements for achievement' : 'User does not meet requirements for achievement',
+            [
+                'user_id' => $user->id,
+                'achievement_id' => $achievement->id,
+                'achievementType' => $achievement->type,
+                'watched_count' => $user->watched()->count(),
+                'target_count' => $achievement->target_count,
+            ]
+        );
+
+        return $hasAchieved;
     }
 }
